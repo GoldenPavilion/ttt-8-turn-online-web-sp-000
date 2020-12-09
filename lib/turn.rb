aa1bb349476b455ce6ddb08140a6fcbae6f71c35
Robert Stine <rcorystine@gmail.com>
2020-12-09 23:14:55 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board,index)
-    move(board, index, character = "X")
+    #move(board, index, character = "X")
     turn(board)
   end
   display_board(board)
