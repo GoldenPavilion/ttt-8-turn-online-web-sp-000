23e57c6c3a2ef1ee781c3770d9f525c8971bbd46
Robert Stine <rcorystine@gmail.com>
2020-12-09 22:05:06 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,7 +10,19 @@ def input_to_index(user_input)
   return (user_input.to_i - 1)
 end
 
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+    return true
+  end
+end
 
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false 
+  else 
+    return true
+  end
+end
 
 def move 
 end
